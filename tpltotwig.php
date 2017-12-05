85270862a1cecbfa78c9a5385a81083958006f07
Сергей Черноудов <s.chernoudov@indatalab.ru>
2017-12-05 18:23:18 +0300

    Read file and produce output.

--- tpltotwig.php
+++ tpltotwig.php
@@ -8,9 +8,31 @@
 
 $options = getopt('', ['dest:']);
 
-if (count($options) == 0 && count($argv) == 0) {
+if (count($argv) - count($options) < 2) {
 	printf('Usage:');
 	return;
 }
 
 $options['src'] = $argv[count($argv) - 1];
+
+run($options);
+
+/**
+ * Opens files and runs conversion.
+ * @param array $options
+ */
+function run ($options) {
+	if (($sfh = fopen($options['src'], "r")) && ($dfh = fopen($options['dest'], "w"))) {
+		try {
+			convert($sfh, $dfh, $options);
+		} catch (Exception $e) {
+			echo $e->getMessage();
+		}
+		fclose($sfh);
+		fclose($dfh);
+	}
+}
+
+function convert ($sfh, $dfh, $options) {
+
+}
