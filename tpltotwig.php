8fa86d807bb8892e66c890d61c29aa0bf6349fcf
Сергей Черноудов <s.chernoudov@indatalab.ru>
2017-12-06 17:35:11 +0300

    Commit.

--- tpltotwig.php
+++ tpltotwig.php
@@ -40,48 +40,99 @@ function run ($options) {
     }
 }
 
+/**
+ * @param string $content
+ * @param array $options
+ * @return string
+ */
 function convert ($content, $options) {
 
     preg_match_all('/'.preg_quote($options['open_tag']).'/', $content, $matches, PREG_OFFSET_CAPTURE);
 
+    $context = new Context();
+
     foreach ($matches[0] as $match) {
         $codeBlockStart = $match[1];
         $codeBlockEnd = strpos($content, $options['close_tag'], $codeBlockStart) + strlen($options['close_tag']);
-        $codeBlock = covertCodeBlock(substr($content, $codeBlockStart, $codeBlockEnd - $codeBlockStart), $options);
+        $codeBlock = convertCode(substr($content, $codeBlockStart, $codeBlockEnd - $codeBlockStart), $context, $options);
     }
 
     return $content;
 }
 
-function covertCodeBlock ($block, $options) {
+/**
+ * @param string $code
+ * @param Context $context
+ * @param array $options
+ */
+function convertCode ($code, $context, $options) {
 
-    $tokens = token_get_all($block);
+    $tokens = token_get_all($code);
 
     $converted = "";
-    foreach ($tokens as $index => $token) {
-        if (is_array($token)) {
-            switch (token_name($token[0])) {
-                case T_OPEN_TAG:
-                case T_CLOSE_TAG:
-                case T_FOREACH:
-                default:
-                    echo token_name($token[0]), PHP_EOL;
-            }
-        } else {
-            switch ($token) {
-                case '{':
-                    echo $token, PHP_EOL;
-                    break;
-                case '}':
-                    echo $token, PHP_EOL;
-                    break;
-                case '(':
-                    echo $token, PHP_EOL;
-                    break;
-                case ')':
-                    echo $token, PHP_EOL;
-                    break;
-            }
-        }
-    }
+
+    fillContext($context, $code, $options);
+}
+
+/**
+ * @param Context $context
+ * @param string $code
+ * @param array $options
+ */
+function fillContext ($context, $code, $options) {
+
+	$tokens = token_get_all($code);
+
+	for ($index = 0, $size = count($tokens) ; $index < $size ; $index++) {
+		$token = $tokens[$index];
+		if (is_array($token)) {
+			switch (token_name($token[0])) {
+				case T_OPEN_TAG:
+				case T_CLOSE_TAG:
+				case T_FOREACH:
+				case T_ECHO:
+					$context->operation = 'echo';
+					break;
+				default:
+					echo token_name($token[0]), PHP_EOL;
+			}
+		} else {
+			switch ($token) {
+				case '{':
+					echo $token, PHP_EOL;
+					break;
+				case '}':
+					echo $token, PHP_EOL;
+					break;
+				case '(':
+					echo $token, PHP_EOL;
+					break;
+				case ')':
+					echo $token, PHP_EOL;
+					break;
+				case ';':
+					echo $token, PHP_EOL;
+					break;
+			}
+		}
+	}
+}
+
+class Context {
+
+	private $_blocks = [];
+
+	function _construct () {}
+
+	function getCurrentBlock() {
+		return current($this->_blocks);
+	}
+
+	function pushBlock($block) {
+		return array_push($this->_blocks, $block);
+	}
+
+	function popBlock() {
+		return array_pop($this->_blocks);
+	}
 }
