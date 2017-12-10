48a57ac64ff2fc1adcc35d38772fa9a6b4e3102a
Сергей Черноудов <s.chernoudov@indatalab.ru>
2017-12-11 02:17:05 +0300

    Commit.

--- tpltotwig.php
+++ tpltotwig.php
@@ -51,46 +51,69 @@ function convert ($content, $options) {
 
     $context = new Context();
 
+    $result = substr($content, 0, strlen($content));
+
     foreach ($matches[0] as $match) {
         $codeBlockStart = $match[1];
         $codeBlockEnd = strpos($content, $options['close_tag'], $codeBlockStart) + strlen($options['close_tag']);
         $codeBlock = convertCode(substr($content, $codeBlockStart, $codeBlockEnd - $codeBlockStart), $context, $options);
+        $result = substr_replace($result, $codeBlock, $codeBlockStart, $codeBlockEnd);
     }
 
-    return $content;
+    return $result;
 }
 
 /**
  * @param string $code
  * @param Context $context
  * @param array $options
+ * @return string
  */
 function convertCode ($code, $context, $options) {
 
-    $tokens = token_get_all($code);
+    $metaInformation = parseCode($context, $code, $options);
 
-    $converted = "";
+    $converted = $metaInformation == NULL ? $code : buildCode($metaInformation);
 
-    fillContext($context, $code, $options);
+    return $converted;
+}
+
+/**
+ * @param $meta
+ * @return string
+ */
+function buildCode ($meta) {
+    $code = NULL;
+    switch ($meta->operation) {
+        case 'echo':
+            $code = buildEcho($meta);
+    }
+    return $code;
+}
+
+function buildEcho($meta) {
+    return '{{ '.$meta->argument. ' }}';
 }
 
 /**
  * @param Context $context
  * @param string $code
  * @param array $options
+ * @return null|object
  */
-function fillContext ($context, $code, $options) {
+function parseCode ($context, $code, $options) {
 
 	$tokens = token_get_all($code);
-
+    $meta = NULL;
 	for ($index = 0, $size = count($tokens) ; $index < $size ; $index++) {
 		$token = $tokens[$index];
 		if (is_array($token)) {
 			switch ($token[0]) {
                 case T_ECHO:
-                    fillContextForEcho($context, $code);
+                    $meta = parseEcho($code);
                     break;
 				case T_FOREACH:
+                    $meta = parseForeach($code);
 				default:
 					echo token_name($token[0]), PHP_EOL;
 			}
@@ -114,16 +137,55 @@ function fillContext ($context, $code, $options) {
 			}
 		}
 	}
+
+	return $meta;
 }
 
-function fillContextForEcho($context, $code) {
+function parseEcho ($code) {
 
     $operation = 'echo';
-    $context->operation = $operation;
+
+    $echo = (object) [
+        'operation' => $operation
+    ];
+
     $operandStartIndex = strpos($code, $operation) + strlen($operation) + 1;
     $operandEndIndex = strpos($code, ';', $operandStartIndex);
-    $context->expression = str_replace('$', '', trim(substr($code, $operandStartIndex, $operandEndIndex - $operandStartIndex)));
+
+    $echo->argument = trim(substr($code, $operandStartIndex, $operandEndIndex - $operandStartIndex));
+
+    return $echo;
 }
+
+function parseForeach ($code) {
+
+    $operation = 'foreach';
+
+    $foreach = (object) [
+        'operation' => $operation
+    ];
+
+    $expressionStartIndex = strpos($code, $operation) + strlen($operation) + 1;
+    $expressionStartIndex = strpos($code, '(', $expressionStartIndex);
+    $expressionEndIndex = strpos($code, ')', $expressionStartIndex);
+
+    $condition = explode('as', trim(substr($code, $expressionStartIndex, $expressionEndIndex - $expressionStartIndex)));
+    $foreach->array_expression = $condition[0];
+    if (strpos($condition, '=>')) {
+        $keyValue = explode('=>', $condition[1]);
+        $foreach->key = trim($keyValue[0]);
+        $foreach->value = trim($keyValue[1]);
+    } else {
+        $foreach->value = trim($condition[1]);
+    }
+
+    return $foreach;
+}
+
+function parseExpression ($code) {
+
+}
+
 
 class Context {
 
